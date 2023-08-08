83d10d177ed07434a04944dbfbd6e44994c70cbe
Lucas Nabesima <lucas.nabesima@gmail.com>
2023-08-08 08:55:48 -0300

    feat: create a list for the letters of chosen word

--- 07-Hangman/hangman.py
+++ 07-Hangman/hangman.py
@@ -1,7 +1,18 @@
 import random
 
+# Step 2
+
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
+
+# Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+display = []
+for letter in chosen_word:
+    display.append("_")
+
+print(display)
 
 while True:
     guess = input("Guess a letter from the word: ").lower()
