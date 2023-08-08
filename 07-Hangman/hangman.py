3278f7ab4f26af0c6ed3489f0075ae1009e59724
Lucas Nabesima <lucas.nabesima@gmail.com>
2023-08-08 13:17:47 -0300

    feat: create a loop to make the user keep guessing

--- 07-Hangman/hangman.py
+++ 07-Hangman/hangman.py
@@ -1,7 +1,5 @@
 import random
 
-# Step 2
-
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 
@@ -12,17 +10,22 @@ display = []
 for letter in chosen_word:
     display.append("_")
 
-print(display)
+end_game = "_" not in display
 
-while True:
-    guess = input("Guess a letter from the word: ").lower()
-    if len(guess) == 1:
+print(display)
+while not end_game:
+    while True:
+        guess = input("Guess a letter from the word: ").lower()
+        if len(guess) == 1:
+            break
+        print("Please input a single letter.")
+        continue
+
+    for index, letter in enumerate(chosen_word):
+        if letter == guess:
+            display[index] = letter
+
+    print(display)
+    if "_" not in display:
+        print("You won the game!")
         break
-    print("Please input a single letter.")
-    continue
-
-for index, letter in enumerate(chosen_word):
-    if letter == guess:
-        display[index] = letter
-
-print(display)
