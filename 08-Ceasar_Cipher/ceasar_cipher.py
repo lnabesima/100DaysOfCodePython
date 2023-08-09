181593d09f92de3a8a211d81a8ff98387db5c351
Lucas Nabesima <lucas.nabesima@gmail.com>
2023-08-09 07:33:02 -0300

    feat: solve step 2 of ceasar_cipher exercise

--- 08-Ceasar_Cipher/ceasar_cipher.py
+++ 08-Ceasar_Cipher/ceasar_cipher.py
@@ -1,13 +1,12 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
-
+alphabet_length = len(alphabet)
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
 def encrypt(plain_text, shift_amount):
-    alphabet_length = len(alphabet)
     encrypted_text = ""
     for letter in plain_text:
         current_letter = alphabet.index(letter)
@@ -16,5 +15,16 @@ def encrypt(plain_text, shift_amount):
     print(encrypted_text)
 
 
-if direction == "encode":
+def decrypt(cipher_text, shift_amount):
+    decrypted_text = ""
+    for letter in cipher_text:
+        current_letter = alphabet.index(letter)
+        shifted_letter = current_letter - shift_amount
+        decrypted_text += alphabet[shifted_letter]
+    print(decrypted_text)
+
+
+if direction == 'encode':
     encrypt(text, shift)
+elif direction == 'decode':
+    decrypt(text, shift)
