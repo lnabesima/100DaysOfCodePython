8e25b950b5524234e0c89d93c234c6d129378432
Lucas Nabesima <lucas.nabesima@gmail.com>
2024-02-02 08:04:58 -0300

    feat: add a loop and a recursion to the calculator

--- 10-Calculator/calculator.py
+++ 10-Calculator/calculator.py
@@ -29,13 +29,25 @@ operations = {
     "/": divide,
 }
 
-num1 = int(input("What's the fist number? "))
-num2 = int(input("What's the second number? "))
+def calculator():
+    num1 = int(input("What's the fist number? "))
+    for symbol in operations:
+        print(symbol)
 
-for symbol in operations:
-    print(symbol)
-    
-operation_symbol = input("Pick an operation from the line above: ")
+    should_continue = True
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = int(input("What's the next number? "))
 
-answer = operations[operation_symbol](num1, num2)
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+        answer = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+        if (
+            input(f"Press [y] to continue calculating with {answer} or type any key to start a new calculation: ")
+            == "y"
+        ):
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+            
+calculator()
